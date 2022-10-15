22effcfb66b826813015cfa7c640feb2102e6954
zettacamp-choirul-annam <choirul.annam@zettacamp.pro>
2022-10-15 22:33:45 +0700

    update & fix

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+// ini sekali aja manggilnya
 require('dotenv').config();
 
-const app = express();
-const port = 3000;
+// import function connectToDB
+const connectToDB = require('./db');
 
+// run function connectToDB
+connectToDB();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
