3bd007a43f563aa09fae9bedd66842f0edcea843
zettacamp-choirul-annam <choirul.annam@zettacamp.pro>
2022-10-16 07:47:10 +0700

    update db.js

--- db.js
+++ db.js
@@ -1,23 +1,18 @@
-
-require('dotenv').config();
 const mongoose = require('mongoose');
  
-let uri = process.env.DB_URI
-    .replace("<user>", process.env.DB_USER)
-    .replace("<password>", process.env.DB_PASS)
-    .replace("<name>", process.env.DB_NAME);
+function connectToDB() {
+    let uri = process.env.DB_URI
+        .replace("<user>", process.env.DB_USER)
+        .replace("<password>", process.env.DB_PASS)
+        .replace("<name>", process.env.DB_NAME);
+    
+    mongoose
+        .connect(uri)
+        .then(() => console.log(`Connected to database`))
+        .catch(err => {
+            console.log('Failed to connect database: ', err.message);
+            mongoose.disconnect();
+        });
+}
 
-// mongoose.connect(uri).catch(error =>{
-//     console.log(error.message);
-//     mongoose.disconnect();
-// });
-
-mongoose.connect(uri).then(
-    () => { 
-        console.log(`Connected to database`);
-    },
-    err => { 
-        console.log(err.message);
-        mongoose.disconnect();
-    }
-  );
+module.exports = connectToDB;
